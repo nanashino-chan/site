fb9f8f52bb7c59b60171605041db7bbe9d3396e6
Nanashino-chan <nanashinochannel774@gmail.com>
2026-05-10 15:50:32 +0900

    Update generate-sitemap.js

--- js/generate-sitemap.js
+++ js/generate-sitemap.js
@@ -2,9 +2,6 @@ const fs = require("fs");
 
 const baseUrl = "https://nanashino-chan.github.io/site";
 
-const today = new Date().toISOString().split("T")[0];
-
-// 手動管理ページ
 const pages = [
   ["", "1.0", "weekly"],
   ["/today.html", "0.97", "weekly"],
@@ -14,47 +11,31 @@ const pages = [
   ["/study.html", "0.94", "weekly"],
   ["/sleep.html", "0.94", "weekly"],
   ["/relax.html", "0.94", "weekly"],
+  ["/tokyo.html", "0.93", "weekly"],
+  ["/cafe.html", "0.93", "weekly"],
+  ["/jazzhop.html", "0.93", "weekly"],
+  ["/synth.html", "0.93", "weekly"],
   ["/music.html", "0.86", "monthly"],
-  ["/faq.html", "0.8", "monthly"]
+  ["/about.html", "0.75", "monthly"],
+  ["/works.html", "0.75", "monthly"],
+  ["/faq.html", "0.8", "monthly"],
+  ["/commerce.html", "0.6", "yearly"],
+  ["/terms.html", "0.4", "yearly"],
+  ["/policy.html", "0.4", "yearly"]
 ];
 
-// pagesフォルダを自動追加
-const pageFiles = fs.readdirSync("./pages");
-
-pageFiles.forEach(file => {
-
-  if (
-    file.endsWith(".html") &&
-    file !== "index.html"
-  ) {
-
-    pages.push([
-      `/pages/${file}`,
-      "0.75",
-      "monthly"
-    ]);
-
-  }
-
-});
+const today = new Date().toISOString().split("T")[0];
 
 const xml = `<?xml version="1.0" encoding="UTF-8"?>
-
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-
 ${pages.map(p => `
-
   <url>
     <loc>${baseUrl}${p[0]}</loc>
     <lastmod>${today}</lastmod>
     <changefreq>${p[2]}</changefreq>
     <priority>${p[1]}</priority>
   </url>
-
 `).join("")}
-
 </urlset>`;
 
 fs.writeFileSync("sitemap.xml", xml);
-
-console.log("Sitemap generated!");
